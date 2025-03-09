61f8e6eab5e83808fadfec5866366829db83b58b
CheesyRobot <ievalt2020@gmail.com>
2025-03-10 01:43:50 +0200

    Animacijos
    Pataisytas judėjimo delay ir įdėta vaikščiojimo animacija. Taip pat įdėta naudojimo (Use) animacija, tačiau kol kas ji neaktyvi

--- PointOfReturnGame/Assets/Scripts/PlayerMovement.cs
+++ PointOfReturnGame/Assets/Scripts/PlayerMovement.cs
@@ -5,19 +5,35 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float movementSpeed = 5f;
     Rigidbody2D rb;
     Vector2 movementDirection;
+    Animator anim;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
 
     void FixedUpdate() 
     {
         rb.linearVelocity = movementDirection.normalized * movementSpeed;
+        MovementAnimation();
+        
+    }
+
+    void MovementAnimation()
+    {
+        if (rb.linearVelocity.magnitude != 0)
+        {
+            anim.SetFloat("MoveX", movementDirection.x);
+            anim.SetFloat("MoveY", movementDirection.y);
+            anim.SetBool("Walking", true);
+        }
+        else
+            anim.SetBool("Walking", false);
     }
 }
