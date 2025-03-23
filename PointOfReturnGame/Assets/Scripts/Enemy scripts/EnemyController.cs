479f18c3f82718da5ef1827a7ac6bd6df6a56532
Rocikm <rokassimkunass@gmail.com>
2025-03-23 13:19:10 +0200

    Priešo animacijos

--- PointOfReturnGame/Assets/Scripts/Enemy scripts/EnemyController.cs
+++ PointOfReturnGame/Assets/Scripts/Enemy scripts/EnemyController.cs
@@ -7,8 +7,9 @@ public class EnemyController : MonoBehaviour
     private Rigidbody2D rb;
     private PlayerAwarenessController playerAwareness;
     private Vector2 targetDirection;
+    Animator anim;
 
-    
+
     [SerializeField] float maxHealth = 10f;
     [SerializeField] float health = 0f;
 
@@ -17,6 +18,7 @@ public class EnemyController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         playerAwareness = GetComponent<PlayerAwarenessController>();
         health = maxHealth;
+        anim = GetComponent<Animator>();
     }
 
 
@@ -24,12 +26,13 @@ public class EnemyController : MonoBehaviour
     {
         UpdateTargetDirection();
         SetVelocity();
+        MovementAnimation();
     }
 
 
     private void UpdateTargetDirection()
     {
-        if(playerAwareness.AwareOfPlayer)
+        if (playerAwareness.AwareOfPlayer)
         {
             targetDirection = playerAwareness.DirectionToPlayer;
         }
@@ -42,7 +45,7 @@ public class EnemyController : MonoBehaviour
 
     private void SetVelocity()
     {
-        if(targetDirection == Vector2.zero)
+        if (targetDirection == Vector2.zero)
         {
             rb.linearVelocity = Vector2.zero;
         }
@@ -56,9 +59,29 @@ public class EnemyController : MonoBehaviour
     public void TakeDamage(float damage)
     {
         health -= damage;
-        if(health <=0)
+        if (health <= 0)
         {
             Destroy(gameObject);
         }
+    }
+
+    void MovementAnimation()
+    {
+        if (rb.linearVelocity.magnitude != 0)
+        {
+            if (Mathf.Abs(rb.linearVelocity.x) > Mathf.Abs(rb.linearVelocity.y))
+            {
+                anim.SetFloat("MoveX", 1 * Mathf.Sign(rb.linearVelocity.x));
+                anim.SetFloat("MoveY", 0);
+            }
+            else
+            {
+                anim.SetFloat("MoveY", 1 * Mathf.Sign(rb.linearVelocity.y));
+                anim.SetFloat("MoveX", 0);
+            }
+            anim.SetBool("Walking", true);
+        }
+        else
+            anim.SetBool("Walking", false);
     }
 }
