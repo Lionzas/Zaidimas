3fcb00fdc0ddca28c757760dc93f06ff635aa944
Rocikm <rokassimkunass@gmail.com>
2025-05-11 19:51:17 +0300

    Elementarus perėjimas į praeities sceną

--- PointOfReturnGame/Assets/Scripts/Interactable Items/TimeMachine.cs
+++ PointOfReturnGame/Assets/Scripts/Interactable Items/TimeMachine.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TimeMachine : MonoBehaviour, IInteractable
 {
@@ -41,6 +42,7 @@ public class TimeMachine : MonoBehaviour, IInteractable
         if (portalState >= 3)
         {
             // put travelling to past here
+            SceneManager.LoadScene("PastWorld");
         }
         else
         {
